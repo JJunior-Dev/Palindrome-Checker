689a36e89261be99fc0add0a330fd28b1d3847ad
JJunior-Dev <jfa_junior@hotmail.com>
2024-05-24 12:12:19 +0200

    added readme > updated result colors

---
+++ README.md
@@ -0,0 +1,6 @@
+Palindrome Checker
+
+
+A <i>palindrome</i> is a word or phrase that can be read the same way forwards and backwards, ignoring punctuation, case, and spacing.
+
+Challange By Free Code Camp.
--- script.js
+++ script.js
@@ -3,19 +3,22 @@ const checkButton = document.getElementById('check-btn');
 const result = document.getElementById('result');
 
 checkButton.addEventListener('click', () => {
-  value = textInput.value;
+  const value = textInput.value;
 
   if (value === '') {
     alert('Please input a value');
-  } else if (textInput.value.length === 1) {
-    result.innerHTML = `<b>${value}</b> is a palindrome.`;
+  } else if (value.length === 1) {
+    result.innerHTML = `<p class="valid"><b>${value}</b> is a palindrome.</p>`;
+    textInput.value = '';
   } else {
     const reversedValue = value.split('').reverse().join('');
     console.log(reversedValue);
     if (value === reversedValue) {
-      result.innerHTML = `<b>${value}</b> is a palindrome.`;
+      result.innerHTML = `<p class="valid"><b>${value}</b> is a palindrome.</p>`;
+      textInput.value = '';
     } else {
-      result.innerHTML = `<b>${value}</b> is not a palindrome.`;
+      result.innerHTML = `<p class="invalid"><b>${value}</b> is not a palindrome.</p>`;
+      textInput.value = '';
     }
   }
 });
