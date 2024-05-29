7aa99ebc6d9da85797d817d280ecb1f99913c41a
JJunior-Dev <jfa_junior@hotmail.com>
2024-05-29 10:20:14 +0200

    upgraded > script code

--- script.js
+++ script.js
@@ -1,33 +1,30 @@
-const textInput = document.getElementById('text-input');
-const checkButton = document.getElementById('check-btn');
-const result = document.getElementById('result');
+const textInput = document.getElementById("text-input");
+const checkButton = document.getElementById("check-btn");
+const result = document.getElementById("result");
 
 function validateInput() {
   const value = textInput.value;
-  
-  if (value === '') {
-    alert('Please input a value');
-  } else if (value.length === 1) {
+  const lowerReplaced = value.toLowerCase().replace(/[^a-z0-9]/g, "");
+
+  if (value == '') {
+    alert("Please input a value");
+  } else if (textInput.value.length == 1) {
     result.innerHTML = `<p class="valid"><b>${value}</b> is a palindrome.</p>`;
     textInput.value = '';
+  } else if (lowerReplaced == [...lowerReplaced].reverse().join("")) {
+    result.innerHTML = `<p class="valid"><b>${value}</b> is a palindrome.</p>`;
+      textInput.value = '';
   } else {
-    const reversedValue = value.split('').reverse().join('');
-    console.log(reversedValue);
-    if (value === reversedValue) {
-      result.innerHTML = `<p class="valid"><b>${value}</b> is a palindrome.</p>`;
+    result.innerHTML = `<p class="invalid"><b>${value}</b> is not a palindrome.</p>`;
       textInput.value = '';
-    } else {
-      result.innerHTML = `<p class="invalid"><b>${value}</b> is not a palindrome.</p>`;
-      textInput.value = '';
-    }
   }
 };
 
 checkButton.addEventListener('click', validateInput)
 textInput.addEventListener('keyup', (e) => {
   const word = e.target.value;
-  const key = e.which || e.keyCode;
-  const isEnterKeyPressed = key === 13;
+  const key = e.key;
+  const isEnterKeyPressed = key === 'Enter';
   if (isEnterKeyPressed) {
       validateInput(word)
       e.target.value = '';
